6f75d1010da9ef58ba47554d8367eecff0074df7
Naveenkumar S <naveenkumar.sanjeevirayan@syncfusion.com>
2025-07-24 14:27:00 +0530

    Added getting started demo

---
+++ SwitchGettingStarted/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.Maui.Buttons;
+
+namespace SwitchGettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSwitchStateChanged(object sender, SwitchStateChangedEventArgs e)
+        {
+            DisplayAlert("Message", "SUCCESS", "OK");
+        }
+    }
+
+}
